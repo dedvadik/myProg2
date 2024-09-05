422bcc75c0e8fe80a0611e31c4843d705668113b
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-09-05 16:07:12 +0300

    delet "Starter" file

--- mainFolder/obj/Debug/net8.0/mainFolder.AssemblyInfo.cs
+++ mainFolder/obj/Debug/net8.0/mainFolder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a8bf55870ddfa01b03ffc4c6f06df006bbd958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0437e74bad25ae891b8c4fdd1e24dce68bda2ba9")]
 [assembly: System.Reflection.AssemblyProductAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
