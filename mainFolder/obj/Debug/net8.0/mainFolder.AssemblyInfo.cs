0e723540531e052dbc910bc63b7ca4951dfa35a7
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-09-05 10:13:43 +0300

    1

--- mainFolder/obj/Debug/net8.0/mainFolder.AssemblyInfo.cs
+++ mainFolder/obj/Debug/net8.0/mainFolder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce117f2596f3f57ec7e60fdf0bd4ed637becebf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d144dc375b2b38f49d5c64135f962cf37d85ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
