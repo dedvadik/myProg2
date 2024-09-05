13a79c90b7683c0394fc6c53e1bdd939a723878f
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-09-05 12:59:51 +0300

    create current Grade

--- mainFolder/Program.cs
+++ mainFolder/Program.cs
@@ -1,7 +1,80 @@
-﻿string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+﻿int currentAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+int[] studentScores = new int[10];
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+string currentStudentLetterGrade = "";
+
+Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
 {
+    string currentStudent = name;
 
-    System.Console.WriteLine($"{name}");
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade;
+
+    string grade(decimal currentStudentGrade)
+    {
+        if (currentStudentGrade >= 97)
+            return "A+";
+        else if (currentStudentGrade >= 93)
+            return "A";
+        else if (currentStudentGrade >= 90)
+            return "A-";
+        else if (currentStudentGrade >= 87)
+            return "B+";
+        else if (currentStudentGrade >= 83)
+            return "B";
+        else if (currentStudentGrade >= 80)
+            return "B-";
+        else if (currentStudentGrade >= 77)
+            return "C+";
+        else if (currentStudentGrade >= 73)
+            return "C";
+        else if (currentStudentGrade >= 70)
+            return "C-";
+        else if (currentStudentGrade >= 67)
+            return "D+";
+        else if (currentStudentGrade >= 63)
+            return "D";
+        else if (currentStudentGrade >= 60)
+            return "D-";
+        else
+            return "F";
+    }
+    {
+        foreach (int score in studentScores)
+        {
+
+            sumAssignmentScores += score;
+
+        }
+
+        currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+
+        Console.WriteLine($"{currentStudent}:\t\t {currentStudentGrade} \t{grade(currentStudentGrade)}");
+    }
 }
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
