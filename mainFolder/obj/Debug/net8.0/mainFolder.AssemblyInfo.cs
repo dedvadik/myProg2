1e486f321dff98be27135b7a698b9a7cce1c4643
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-09-05 15:34:23 +0300

    Написание всего кода для подсчёта среднего значения учащийхся класса

--- mainFolder/obj/Debug/net8.0/mainFolder.AssemblyInfo.cs
+++ mainFolder/obj/Debug/net8.0/mainFolder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2be46c1e600525585eee351697d1ac19e09231")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a09a5d92296c4524055880d9afb3415a1a38fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mainFolder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
